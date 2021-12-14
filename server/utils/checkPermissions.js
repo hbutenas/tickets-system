1d1ffddef0dd62b7353ffee4f0c6857d638684c8
Henrikas <butenashenrikas@gmail.com>
2021-12-14 17:59:19 +0200

    basic validation to avoid accessing other users tickets

---
+++ server/utils/checkPermissions.js
@@ -0,0 +1,16 @@
+const { StatusCodes } = require('http-status-codes');
+
+const checkPermissions = (res, requestUser, resourceUserId) => {
+  if (requestUser.userId === resourceUserId) return;
+  if (
+    requestUser.role === 'Owner' ||
+    requestUser.role === 'Admin' ||
+    requestUser.role === 'IT_SUPPORT'
+  )
+    return;
+  res
+    .status(StatusCodes.UNAUTHORIZED)
+    .json({ message: 'You are not authorized to access this route' });
+};
+
+module.exports = checkPermissions;
