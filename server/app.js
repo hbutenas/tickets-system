a1183915cb213301c12aa375a10154b98af347b3
Henrikas <butenashenrikas@gmail.com>
2021-12-14 17:58:15 +0200

    imported middlewares

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('express-async-errors');
 const express = require('express');
 const app = express();
 
@@ -10,6 +11,10 @@ const ticketsRouter = require('./routes/ticketsRouter');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 
+// middlewares
+const routeNotFoundMiddleware = require('./middlewares/routeNotFound');
+const errorHandlerMiddleware = require('./middlewares/errorHandler');
+
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(cookieParser(process.env.JWT_SECRET));
@@ -17,6 +22,9 @@ app.use(cookieParser(process.env.JWT_SECRET));
 // routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/tickets', ticketsRouter);
+
+app.use(routeNotFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
