543e42f0fe3104ac4da16c1edbccae34429ca751
Henrikas <butenashenrikas@gmail.com>
2021-12-15 13:55:34 +0200

    created custom error handler middleware to validate errors

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,10 +1,17 @@
 const { StatusCodes } = require('http-status-codes');
 const errorHandler = async (err, req, res, next) => {
+  console.log(err);
   // set default
   let customError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || 'Something went wrong try again later',
   };
+
+  // duplicate value
+  if (err.code === '23505') {
+    customError.msg = `Duplicate value entered for ${err.constraint}`;
+    customError.statusCode = 400;
+  }
 
   return res.status(customError.statusCode).json({ message: customError.msg });
 };
