727b61bd5021e27c16af9858620784094ed7345d
Henrikas <butenashenrikas@gmail.com>
2021-12-16 17:22:35 +0200

    adapted settings for knex library

--- server/database/connectDB.js
+++ server/database/connectDB.js
@@ -1,11 +1,22 @@
-const Pool = require('pg').Pool;
-
-const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+const pool = require('knex')({
+  client: 'pg',
+  connection: {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+  },
 });
+
+// const Pool = require('pg').Pool;
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_NAME,
+// });
 
 module.exports = pool;
