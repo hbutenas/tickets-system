68f588a364dd67331e37dd6fff75de5238ec0504
Henrikas <butenashenrikas@gmail.com>
2021-12-16 17:20:40 +0200

    created new error type

--- server/errors/index.js
+++ server/errors/index.js
@@ -2,10 +2,12 @@ const CustomError = require('./CustomError');
 const UnauthorizedError = require('./UnauthorizedError');
 const UnauthenticatedError = require('./UnauthenticatedError');
 const BadRequestError = require('./BadRequestError');
+const NotFoundError = require('./NotFoundError');
 
 module.exports = {
   CustomError,
   UnauthorizedError,
   UnauthenticatedError,
   BadRequestError,
+  NotFoundError,
 };
